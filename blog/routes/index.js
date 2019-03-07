25fa0fb1438bdecb897b1bfbeebb0e3118dbbbc0
cfr2ak <cfr2ak@gmail.com>
2019-03-07 12:39:02 +0900

    upload to server

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '/../views/index.html'));
 });
+
+router.get('/tutorials', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../views/articles.html'));
+}) ;
 
 module.exports = router;
